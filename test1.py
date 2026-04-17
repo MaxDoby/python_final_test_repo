480f87ad041cb680ba37b67c3a1a70a1fa491e3b
Maxim Dobinda <maxim.benid@gmail.com>
2026-04-17 19:53:31 +0300

    add new solutions

--- test1.py
+++ test1.py
@@ -1,12 +1,78 @@
-print(-6+8*6)
-print((-44+8)%5)
-print(19+-3*2/8)
-print(5+20/3*2-8%4)
+# print(-6+8*6)
+# print((-44+8)%5)
+# print(19+-3*2/8)
+# print(5+20/3*2-8%4)
+#
+#  # Ex2
+#
+# num1 = float(input("Primul numar: "))
+# num2 = float(input("Al doilea numar: "))
+# print("Rezultatul inmultirii:", num1 * num2)
+#
+# # Ex3
+#
+# num1 = float(input("Primul numar: "))
+# num2 = float(input("Al doilea numar: "))
+# print("Rezultatul adunarii:", num1 + num2)
+# print("Rezultatul scaderii:", num1 - num2)
+# print("Rezultatul inmultirii:", num1 * num2)
+# print("Rezultatul impartirii:", num1 / num2)
+# print("Rezultatul restul impartirii:", num1 % num2)
 
- # Ex2
+# Ex 4
 
-num1 = float(input("Primul numar: "))
-num2 = float(input("Al doilea numar: "))
-print("Rezultatul inmultirii:", num1 * num2)
+# num1 = int(input("Primul numar: "))
+# num2 = int(input("Al doilea numar: "))
+# if (num1 > num2):
+#     print(f'Numarul {num1} este mai mare ca {num2}')
+# else:
+#     print(f'Numarul {num2} este mai mare ca {num1}')
 
-# Ex3
+# Ex 5
+
+# password = input('Introduceti parola: ')
+# print(f'Parola introdusa este: {password}')
+
+# Ex 6
+
+# for i in range(1, 100):
+#     if i % 2 == 0:
+#         print(i)
+
+# ex 7
+
+# numarIntrodus = int(input('Introduceti un numar: '))
+# if numarIntrodus % 2 == 0:
+#     print(f'Numarul {numarIntrodus} este un numar par.')
+# if numarIntrodus % 2 != 0:
+#     print(f'Numarul {numarIntrodus} este un numar impar.')
+
+# Ex 8
+
+# oraIntrodusa = int(input('Introduceti (in format 0-24h) ora zilei: '))
+# if 5 <= oraIntrodusa < 12:
+#     print('Este dimineata')
+# elif 12 <= oraIntrodusa < 17:
+#     print('Este amiaza')
+# elif 17 <= oraIntrodusa < 21:
+#     print('Este seara')
+# elif (21 <= oraIntrodusa <= 24) or (0 <= oraIntrodusa < 5):
+#     print('Este noapte')
+
+# Ex 9
+
+# luna = int(input("Introduceti numarul lunii (1-12): "))
+# if luna in [12, 1, 2]:
+#     print("Iarna")
+# elif 3 <= luna <= 5:
+#     print("Primavara")
+# elif 6 <= luna <= 8:
+#     print("Vara")
+# elif 9 <= luna <= 11:
+#     print("Toamna")
+
+# Ex 10
+
+# text_pentru_modificare = 'eu astazi scriu examen'
+# print(text_pentru_modificare.upper())
+
